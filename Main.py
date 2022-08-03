b6b1069d268f31e0d98819d4bfceef8bde55aca5
saidiya1021 <109952168+saidiya1021@users.noreply.github.com>
2022-08-03 23:47:22 +0530

    commit1

--- Main.py
+++ Main.py
@@ -1,24 +1,56 @@
 class MyCircularQueue:
     def __init__(self, size: int):
-        # Write code here
+         self.queue = [0] * size
+        self.size = size
+        self.front = -1
+        self.rear = -1
 
     def enqueue(self, value: int) -> bool:
-        # Write code here
+        if not is_full():
+            if self.front == -1:
+                self.front = 0
+                self.rear = 0            
+            else:
+                self.rear = (self.rear + 1) % self.size
+            self.queue[self.rear]=value
+            return True
+        else:
+            return False
 
     def dequeue(self) -> bool:
-        # Write code here
+        if not self.is_empty():            
+            if self.front == self.rear:
+                self.front = -1
+                self.rear = -1
+            else:
+                self.front = (self.front+1)%self.size
+            return True
+        else:
+            return False
 
     def get_front(self) -> int:
-        # Write code here
+        if not self.is_empty():
+            return self.queue[self.front]
+        else:
+            return -1
 
     def get_rear(self):
-        # Write code here
+        if not self.is_empty():
+            return self.queue[self.rear]
+        else:
+            return -1
 
     def is_empty(self):
-        # Write code here
+        if self.front == -1:
+            return True
+        else:
+            return False
 
     def is_full(self):
-        # Write code here
+        if (self.front == 0 and self.rear == self.size-1) or self.front == (self.rear+1)%self.size:
+            return True
+        else:
+            return False
 
 
 # Do not change the following code
